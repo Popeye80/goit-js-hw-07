eec27b1149b652add2d8f0d52b67ba147b13898a
Vladimir Pavlov <91782115+Popeye80@users.noreply.github.com>
2022-04-09 20:10:42 +0300

    1-я часть 7-й

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,64 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createImagesCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener("click", onClickGalleryImageOpen);
+
+// Создаем галерею
+
+function createImagesCardsMarkup(galleryItems) {  
+    return galleryItems.map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+    })
+    .join('');
+           }
+  
+
+// 
+
+function onClickGalleryImageOpen(evt) {
+  const { target } = evt;
+  const isTargetImage = target.classList.contains("gallery__image");
+
+  if (isTargetImage) {
+    evt.preventDefault();
+    let isModalOpen = false;
+
+    const instance = basicLightbox.create(
+      `
+         <img src="${evt.target.dataset.source}" width="800" height="600">
+      `,
+      { onClose: () => document.removeEventListener("keyup", closeModal) }
+    );
+
+    isModalOpen = instance.show();
+
+    if (isModalOpen) {
+      document.addEventListener("keyup", closeModal);
+    }
+
+    // Закрытие модалки по Esc
+
+    function closeModal(evt) {
+      if (evt.key === "Escape") {
+        instance.close();
+      }
+    }
+  }
+}        
  
-
-
-    
 
